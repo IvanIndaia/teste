c9bb55dac2ae0383a10c0fff4c52b57c1ffd3eb8
Ivan Oliveira <79813537+IvanIndaia@users.noreply.github.com>
2022-06-06 22:08:28 -0300

    Today

---
+++ Liberta_2022c/script.js
@@ -0,0 +1,22 @@
+
+let pls = document.getElementsByClassName("placar-E")
+
+
+
+// alert("O valor 1 é: ", pl1)
+
+
+
+function apostar() {
+    // console.log(placarE.value)
+    // placarE.value[1]
+
+    // console.log(pls[5].value)
+    // console.log(pls)
+
+    for (var i = 0; i < pls1.length; i++){
+        // console.log(pls[i].textContent)
+        console.log(pls1[i].value)
+        }
+    }
+    
