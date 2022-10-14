5f822f2fa2d1002a56bf9b395ad28a3a4b0d233e
Ivan Oliveira <79813537+IvanIndaia@users.noreply.github.com>
2022-10-14 17:41:00 -0300

    Temperamentos....

---
+++ Temperamentos/javascript/javaScript.js
@@ -0,0 +1,13 @@
+
+function starter() {
+    // let jogador1 = prompt("Digite o nome do Jogador 1")
+    // let jogador2 = prompt("Digite o nome do Jogador 2")
+
+    // let jog1 = document.getElementById("j1")
+    // let jog2 = document.getElementById("j2")
+
+    // jog1.innerHTML = `${jogador1}`
+    // jog2.innerHTML = `${jogador2}`
+
+    document.getElementById("startTempera").style.display = 'none'
+}
