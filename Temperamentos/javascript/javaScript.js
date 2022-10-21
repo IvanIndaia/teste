fb1af2dfae13e4ec5f9249f0dfa57a6d91014b06
Ivan Oliveira <79813537+IvanIndaia@users.noreply.github.com>
2022-10-21 17:07:18 -0300

    temperamentosssss

--- Temperamentos/javascript/javaScript.js
+++ Temperamentos/javascript/javaScript.js
@@ -1,22 +1,58 @@
 
-function starter() {
-    // let jogador1 = prompt("Digite o nome do Jogador 1")
-    // let jogador2 = prompt("Digite o nome do Jogador 2")
+let S = 0
+let N = 0
+let SN = 0
 
-    // let jog1 = document.getElementById("j1")
-    // let jog2 = document.getElementById("j2")
+function start() {
 
-    // jog1.innerHTML = `${jogador1}`
-    // jog2.innerHTML = `${jogador2}`
+    let q1 = document.getElementById("q1a")
+    let q2 = document.getElementById("q2a")
+    let q3 = document.getElementById("q3a")
+    let q4 = document.getElementById("q4a")
+    let q5 = document.getElementById("q5a")
+    let q6 = document.getElementById("q6a")
+    let q7 = document.getElementById("q7a")
+    let q8 = document.getElementById("q8a")
+    let q9 = document.getElementById("q9a")
+    let q10 = document.getElementById("q10a")
 
-    document.getElementById("startTempera").style.display = 'none'
+    let quest = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10]
+
+    // let x = 'q'+ 1
+
+    // quest.forEach(function(n) {
+    //     console.log(n)
+    // }
+
+    quest.forEach(n => {
+        console.log(n.value)
+    })
+
+
+    // for (let i = 1; i<=2; i++){
+    //     let x = 'q'+i
+    //     alert(x.charAt.)
+    // }
+
+
+    // if (s.checked) {
+    //     alert(ss)
+    // } else {
+    //     alert('Não está selecionado')
+    // }
 }
 
-const btn = document.querySelector('#send')
+    // console.log(radioBtns)
+    // radioBtns.forEach(radioBtn => {"change", console.log(radioBtn.value)})
 
-btn.addEventListener("click", function(e){
-    e.preventDefault()
-    const question1 = document.getElementsByName('q1')
-    let que1 = question1.value
-    console.log(que1)
-})
+	// let findSelected = () => {
+    //     let sel = document.getElementById("q1")
+    //     let selected = sel.value
+	// 	console.log(selected)
+	// }
+
+	// radioBtns.forEach(radioBtn => {
+	// 	radioBtn.addEventListener("change", findSelected)
+	// })
+	// findSelected()
+
