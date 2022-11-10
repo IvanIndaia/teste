2bbeb836ec0c6c93f81c72ecae4bf9a040abacc4
Ivan <ivan.indaia@gmail.com>
2022-11-09 23:10:10 -0300

    boot-Inter

---
+++ Bootcamp-Inter/Classes/Primeiros passos com JS/aula.js
@@ -0,0 +1,15 @@
+
+
+
+const precoCombustivel = 5.79
+const kmPorLitro = 10
+const distKm = 100
+
+const litrosViagem = distKm / kmPorLitro
+const valorViagem = litrosViagem * precoCombustivel
+
+
+
+
+
+console.log('A distancia de ' + distKm + 'KM gastei: R$ ' + valorViagem)
