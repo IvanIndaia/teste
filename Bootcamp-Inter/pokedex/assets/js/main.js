d40a6256f1d07be1dc7572c0ca6141d602f15b8d
IvanIndaia <ivan.indaia@gmail.com>
2022-12-07 22:44:03 -0300

    Pokemon-007

--- Bootcamp-Inter/pokedex/assets/js/main.js
+++ Bootcamp-Inter/pokedex/assets/js/main.js
@@ -1,1 +1,40 @@
 console.log('Sucesso!')
+
+// function convertPokemonTypesToLi(pokemonTypes) {
+//     return pokemonTypes.map((typeSlot) => `<li class="type">${type.name}</li>`)
+// }
+
+function convertPokemonToHTML(pokemon) {
+    return `
+    <li class="pokemon ${pokemon.type}">
+        <span class="number">#${pokemon.number}</span>
+        <span class="name">${pokemon.name}</span>
+
+        <div class="detail">
+            <ol class="types">
+                ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+                <li class="type">poson</li>
+            </ol>
+
+            <img src="${pokemon.photo}" alt="${pokemon.name}">
+        </div>
+    </li>
+    `
+}
+
+const pokeOl =  document.getElementById('pokeId')
+
+
+pokeAPI.getPokemons().then((pokemonList = []) => {
+        
+    pokeOl.innerHTML += pokemonList.map(convertPokemonToHTML).join('')
+
+    // OU use o FOR - Mais códigos
+    // for (let i = 0; i < pokemonList.length; i++) {
+    //     const poke = pokemonList[i];
+    //     console.log(poke)
+    //     listItems.push(convertPokemonToHTML(poke))
+    // }
+
+})
+.finally(() => console.log("Requisição Concluída!"))   
