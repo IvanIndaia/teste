c4332e40c32a104587f2e35756f8a25249bc2578
IvanIndaia <ivan.indaia@gmail.com>
2022-12-21 17:08:13 -0300

    Update App.js

--- desafio01-calculadora/src/App.js
+++ desafio01-calculadora/src/App.js
@@ -102,16 +102,16 @@ const App = () => {
       <Content>
         <Input value={currentNumber} />
         <Row>
-          <Button label="*" onClick={handleMultNumbers} />
-          <Button label="/" onClick={handleDivNumbers} />
           <Button label="c" onClick={handleOnClear} />
-          <Button label="." />
+          <Button label="0" onClick={() => handleAddNumber('0')} />
+          <Button label="=" onClick={handleEquals} />
+          <Button label="/" onClick={handleDivNumbers} />
         </Row>
         <Row>
           <Button label="7" onClick={() => handleAddNumber('7')} />
           <Button label="8" onClick={() => handleAddNumber('8')} />
           <Button label="9" onClick={() => handleAddNumber('9')} />
-          <Button label="-" onClick={handleMinusNumbers} />
+          <Button label="*" onClick={handleMultNumbers} />
         </Row>
         <Row>
           <Button label="4" onClick={() => handleAddNumber('4')} />
@@ -123,7 +123,7 @@ const App = () => {
           <Button label="1" onClick={() => handleAddNumber('1')} />
           <Button label="2" onClick={() => handleAddNumber('2')} />
           <Button label="3" onClick={() => handleAddNumber('3')} />
-          <Button label="=" onClick={handleEquals} />
+          <Button label="-" onClick={handleMinusNumbers} />
         </Row>
       </Content>
     </Container>
