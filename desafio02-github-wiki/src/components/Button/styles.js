a050e131da223d720e473409f4a576b1b394ff51
IvanIndaia <ivan.indaia@gmail.com>
2022-12-22 23:18:04 -0300

    Aulas React

---
+++ desafio02-github-wiki/src/components/Button/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+    background-color: #FAFAFA;
+    border: 1px solid #FAFAFA;
+    border-radius: 5px;
+
+    height: 40px;
+    width: 80%;
+    margin: 5px;
+
+    &:hover {
+        cursor: pointer;
+        background-color: #FAFAFA40;
+    }
+
+    `
---
+++ desafio02_OLD/src/components/Input/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { InputContainer } from './styles'
+
+function Input() {
+    return (
+        <InputContainer>
+            <Input />
+        </InputContainer>
+    )
+}
+
+export default Input
