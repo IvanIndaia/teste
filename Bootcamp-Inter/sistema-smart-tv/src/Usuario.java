40bcb9961e1a5c4d87366e1db3b33a8fa968ac05
IvanIndaia <ivan.indaia@gmail.com>
2023-01-28 19:47:52 -0300

    SmartTv

---
+++ Bootcamp-Inter/sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,14 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+
+        SmartTV smartTV = new SmartTV();
+        
+        smartTV.ligar();
+        smartTV.mudarCanal(12);
+
+        System.out.println("TV ligada: " + smartTV.ligada);
+        System.out.println("No canal: " + smartTV.canal);
+        System.out.println("Com volume: " + smartTV.volume);
+
+    }
+}
