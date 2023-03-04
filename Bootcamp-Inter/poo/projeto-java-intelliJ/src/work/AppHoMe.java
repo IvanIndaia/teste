f769c56c489c01c776f87362bcecb28927d9947f
IvanIndaia <ivan.indaia@gmail.com>
2023-03-04 06:29:46 -0300

    up home

---
+++ Bootcamp-Inter/poo/projeto-java-intelliJ/src/work/AppHoMe.java
@@ -0,0 +1,32 @@
+package work;
+
+public class App {
+    public static void main(String[] args){
+<<<<<<< HEAD
+//        Carror carro1 = new Carror();
+//
+=======
+        Carro carro1 = new Carro();
+
+>>>>>>> 04f93155ceff3c0b710b855b7dcb15df083f48bc
+//        carro1.setCor("Azul");
+//        carro1.setModelo("Prisma");
+//        carro1.setCapacidadeTanque(60);
+//
+//        System.out.println(carro1.getCor());
+//        System.out.println(carro1.getModelo());
+//        System.out.println(carro1.getCapacidadeTanque());
+//        System.out.println("R$ "+carro1.totalValorTanque(4.5));
+//
+//        System.out.println();
+//        carro1.setCor("Prata");
+//        carro1.setModelo("Mercedes");
+//        carro1.setCapacidadeTanque(69);
+//
+//        System.out.println(carro1.getCor());
+//        System.out.println(carro1.getModelo());
+//        System.out.println(carro1.getCapacidadeTanque());
+//        System.out.println("R$ "+carro1.totalValorTanque(6.5));
+
+    }
+}
